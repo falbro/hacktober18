a92e51a80222978685ae2abb07502e2e127f045f
falbro <alfalah.ibrahim@gmail.com>
2020-10-11 04:58:50 +0700

    Delete konv_km_ke_mil.py

--- konv_km_ke_mil.py
+++
@@ -1,9 +0,0 @@
-# Input kilometer
-kilometer = float(input("Masukkan nilai dalam kilometer: "))
-
-# Faktor konversi
-fak_konv = 0.621371
-
-# Konversi ke mil
-mil = kilometer * fak_konv
-print('%0.2f kilometer is equal to %0.2f mil' %(kilometer,mil))
