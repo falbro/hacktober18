3eac777dca8e3b762d78e8b3c9e582b5577f6c16
falbro <alfalah.ibrahim@gmail.com>
2020-10-11 04:50:09 +0700

    Add km to mil
    Percubaan

---
+++ km ke mil.py
@@ -0,0 +1,9 @@
+# Input kilometer
+kilometer = float(input("Masukkan nilai dalam kilometer: "))
+
+# Faktor konversi
+fak_konv = 0.621371
+
+# Konversi ke mil
+mil = kilometer * fak_konv
+print('%0.2f kilometer is equal to %0.2f mil' %(kilometer,mil))
